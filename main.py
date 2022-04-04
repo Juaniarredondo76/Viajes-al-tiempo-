588f1582a1526fa69f682f9b477e45cf5c9f511b
401 <401@academicos.cesde.edu.co>
2022-04-04 15:05:00 -0500

    NTG001 se crea una nueva carpeta desarrollando el while en forma de objetos y arreglos (LISTAS & DICCIONARIOS)

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-#saludando
-print("hola cabrones")
+#LISTA==AREGLOS[]
+#dICCIONARIO EN PYTHON==OBJETOS {}
+#SE HABLA DE ATRIBUTOS ==KEYS/LLAVES 
+
+'''
+#arreglo
+estudiantes= []
+#objetos
+estuduante={}
+'''
+
+estudiantes={
+    'nombre':"Juana",
+    'edad':19,
+    'EsHinchaVerdolaga':True
+
+}
+print(estudiantes)
+print(estudiantes['edad'])
+estudiantes['cedula']=1001578277
+print(estudiantes)
